ef104b9d2d068e661c4d41f5d67026a99825fb46
ENCR2 <qwe123279@gmail.com>
2025-03-17 17:05:16 +0900

    'commit'

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from priority_queue import PriorityQueue
+"""This module demonstrates the usage of the PriorityQueue class."""
+
+from priority_queue import PriorityQueue 
 def main():
+    """
+    This function demonstrates the usage of the PriorityQueue class.
+    """
     pq = PriorityQueue()
-  
+
     pq.push("Task 1", 3)
     pq.push("Task 2", 1)
     pq.push("Task 3", 2)
-  
+
     print(pq.pop())
     print(pq.pop())
     print(pq.pop())
