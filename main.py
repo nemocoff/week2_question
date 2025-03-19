04bd6cd1efaee882d05155e1eea36d59d538d60a
nemocoff <encree123279@gmail.com>
2025-03-19 12:31:01 +0900

    commit

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 """This module demonstrates the usage of the PriorityQueue class"""
 
-from priority_queue import PriorityQueue 
+from priority_queue import PriorityQueue
 def main():
-    """
-    This function demonstrates the usage of the PriorityQueue class
-    """
-    pq = PriorityQueue()
+  """
+  This function demonstrates the usage of the PriorityQueue class
+  """
+  pq = PriorityQueue()
 
-    pq.push("Task 1", 3)
-    pq.push("Task 2", 1)
-    pq.push("Task 3", 2)
+  pq.push("Task 1", 3)
+  pq.push("Task 2", 1)
+  pq.push("Task 3", 2)
 
-    print(pq.pop())
-    print(pq.pop())
-    print(pq.pop())
+  print(pq.pop())
+  print(pq.pop())
+  print(pq.pop())
 
 if __name__ == "__main__":
-    main()
+  main()
